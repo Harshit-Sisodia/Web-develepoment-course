0badd2345b0f7ebe3437f003bb1d3f572b2e6227
Harshit-Sisodia <harshitsisodia789@gmail.com>
2024-05-14 14:04:44 +0530

    update

---
+++ Day3/new.js
@@ -0,0 +1,7 @@
+console.log("Hello world")
+console.log("code is running kind of good")
+console.log("code is running at very low efficiency")
+
+
+
+
---
+++ Day3/script.js
@@ -0,0 +1,23 @@
+
+alert("hello World");
+
+console.log("code is running")
+console.log("code is running kind of good")
+console.log("code is running at very low efficiency")
+
+var a = prompt("Enter the number")
+
+console.log("your number is " +a)
+
+var isTrue=confirm("do you want to blast your computer or not")
+
+if (isTrue) {
+    console.log("The computer is blasting in 3..2..1")
+}
+else {
+    console.log("The computer is not blasting")
+}
+
+document.title= "hey i am good"
+
+document.body.style.backgroundColor= "red" 
